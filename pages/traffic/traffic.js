af371ed2e7ce8e9c3e8c072581cbdd48e50998f0
fenglunli1997 <fenglunli1997@gmail.com>
2019-04-29 16:04:03 +0800

    第一次远程提交
    所有的前端页面初步完工；地图功能全部实现无需再改动

--- pages/traffic/traffic.js
+++ pages/traffic/traffic.js
@@ -1,9 +1,9 @@
 // pages/Traffic/Traffic.js
+// var QQMapWX = require('../../libs/qqmap-wx-jssdk1.0/qqmap-wx-jssdk.js');
+// var qqmapsdk;
 
 Page({
   data: {
-    // color: ['blue', 'black', 'black', 'black'],
-    // border: [1, 0, 0, 0],
     name: ['当前位置',' '],
     markers: [{
       latitude: 0,
@@ -45,30 +45,6 @@ Page({
       longitude: this.data.markers[0].longitude,
     })
   },
-  
-  // initData: function (id) {
-  //   if (id == 2) {
-  //     var a = "polyline.points[0].latitude";
-  //     var b = "polyline.points[0].longitude";
-  //     var c = "polyline.points[1].latitude";
-  //     var d = "polyline.points[1].longitude";
-  //     this.setData({
-  //       [a]: this.data.markers[0].latitude,
-  //       [b]: this.data.markers[0].longitude,
-  //       [c]: this.data.markers[0].latitude,
-  //       [d]: this.data.markers[0].longitude,
-  //     });
-  //   } else{
-  //     var e = "polyline.points[" + id + "].latitude";
-  //     var f = "polyline.points[" + id + "].longitude";
-  //     this.setData({
-  //       [e]: this.data.markers[id].latitude,
-  //       [f]: this.data.markers[id].longitude,
-  //     });
-  //   }
-  //   // console.log(this.data.polyline.points[0].latitude + "initData0");
-  //   // console.log(this.data.polyline.points[1].latitude + "initData1");
-  // },
 
   locate: function(e){
     var that = this;
@@ -83,7 +59,6 @@ Page({
           [b]: res.latitude,
           [c]: res.longitude,
         });
-        // that.initData(no);
       },
     });
   },
@@ -96,20 +71,8 @@ Page({
     })
   },
 
-  // choose: function (e) {
-  //   var id = e.currentTarget.dataset.id;
-  //   var arr = 'color[' + id + ']';
-  //   var ar = 'border[' + id + ']';
-  //   this.setData({ [arr]: 'blue' });
-  //   this.setData({ [ar]: 1 });
-  //   for (var i = 0; i < 4; i++) {
-  //     if (i != id) {
-  //       var a = 'color[' + i + ']';
-  //       var b = 'border[' + i + ']';
-  //       this.setData({ [a]: 'black' });
-  //       this.setData({ [b]: 0 });
-  //     }
-  //   }
+  // search: function(){
+  //   this.route();//
   // },
 
   /**
@@ -117,17 +80,37 @@ Page({
    */
   onLoad: function (options) {
 
+    // // 实例化API核心类
+    // qqmapsdk = new QQMapWX({
+    //   key: '4INBZ-PRVRU-5WUVM-B5CDQ-FGXAS-DGBKA'
+    // });
+
     wx.getLocation({
       success: (res) => {
         this.setData({
           'markers[0].latitude': res.latitude,
           'markers[0].longitude': res.longitude
         });
-        // console.log(this.data.markers[0].latitude + "success");
       },
     });
 
-    // this.initData(2);
-
   },
+
+  // onShow: function () {
+  //   // 调用接口
+  //   qqmapsdk.search({
+  //     keyword: '酒店',
+  //     success: function (res) {
+  //       console.log(res);
+  //     },
+  //     fail: function (res) {
+  //       console.log(res);
+  //     },
+  //     complete: function (res) {
+  //       console.log(res);
+  //     }
+  //   });
+  // },
+  
+  
 })
